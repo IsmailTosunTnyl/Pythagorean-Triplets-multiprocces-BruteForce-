c6aa852d4751b971a9fe1221d254f42e4b66df6b
itosun <ismailtosuntnyl@gmail.com>
2021-12-27 16:34:48 +0300

    final

--- main.py
+++ main.py
@@ -18,32 +18,37 @@ class TicToc:
 
 def triplets(start_point, end_point, n, increase_amount, shared_pythagorean_triplets):
     for a in range(start_point, end_point, increase_amount):
-        for b in range(a, n):
-            for c in range(b+1, n):
+        for b in range(a + 1, n):
+            for c in range(b + 1, n):
                 if a ** 2 + b ** 2 == c ** 2:
                     shared_pythagorean_triplets.append([a, b, c])
 
 
 if __name__ == '__main__':
-    n = 100
-    number_per_process = int(n / os.cpu_count())
-    print(number_per_process)
-    shared_pythagorean_triplets = Manager().list()
-    shared_pythagorean_triplets.append(1)
+    timer = TicToc()
 
+    timer.tic()
+    n = 1000
+    cpu_count = os.cpu_count()
+
+    number_per_process = int(n / cpu_count)
+    shared_pythagorean_triplets = Manager().list()
     processes = list()
 
-    for i, start_point in zip(range(1, os.cpu_count() + 1)[::-1], range(1, os.cpu_count() + 1)):
-        end_point = n - os.cpu_count() + i
+    for i, start_point in zip(range(1, cpu_count + 1)[::-1], range(1, cpu_count + 1)):
+        end_point = n - cpu_count + i
         processes.append(Process(target=triplets,
-                                 args=(start_point, end_point, n, number_per_process, shared_pythagorean_triplets)))
+                                 args=(start_point, end_point, n, cpu_count, shared_pythagorean_triplets)))
 
     for process in processes:
         process.start()
+
     for process in processes:
         process.join()
 
-    for ar in shared_pythagorean_triplets:
-        print(ar)
+    for tri in shared_pythagorean_triplets:
+        print(f"a:{tri[0]} , b:{tri[1]} , c:{tri[2]}")
 
-    print(len(shared_pythagorean_triplets))
+    print("Total numbers of triplets :", len(shared_pythagorean_triplets))
+    print("Number per CPU:", number_per_process)
+    print("Time", timer.toc())
